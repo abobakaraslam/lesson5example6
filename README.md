fd09f6f230cbe19e825a40093a9a78b4412a1ce9
abobakaraslam <m.abobakar.aslam@gmail.com>
2024-03-24 12:00:16 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# lesson5example6
+ Example 6 Lesson 5 Basic Codes for Tailwind CSS in React
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/* File: App.jsx */
+function App() {
+  return (
+    <div>
+      <h1 className="bg-black text-white hover:bg-slate-500 hover:text-black">
+        Some text
+      </h1>
+      <h1 className="bg-black text-white sm:hover:bg-red-700 lg:hover:bg-green-700">
+        Heading with sm:hover:
+      </h1>
+      <h1 className="bg-black text-white hover:sm:bg-yellow-800 hover:lg:bg-cyan-700">
+        hover:sm:
+      </h1>
+    </div>
+  );
+}
+export default App;
